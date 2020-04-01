1e0a1b88e5886e138d00367cfb3b1c62ddcc88b6
Erkko Mäkinen <erccco@gmail.com>
2020-04-01 19:40:49 +0300

    build lisätty

---
+++ build/precache-manifest.be6f4fb90758ba0cc75917b0e5bea39c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0688760b268ec58143504bdf08ae431f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "363435274400714fad45",
+    "url": "/static/js/2.fde9b966.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fde9b966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3a02a150d8bbc4fd0ac",
+    "url": "/static/js/main.0e6d92ff.chunk.js"
+  },
+  {
+    "revision": "4caee76f79619d3bb41a",
+    "url": "/static/js/runtime-main.92e7eacc.js"
+  }
+]);
